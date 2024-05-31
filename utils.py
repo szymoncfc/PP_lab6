38b9613b452d416f6b11793d9efab762daab9c42
szymoncfc <szymon_cfc@o2.pl>
2024-05-31 23:17:52 +0200

    fix

--- utils.py
+++ utils.py
@@ -1,12 +1,31 @@
-# przykladowe funkcje utils .py
-def add (a: int , b: int) -> int:
+"""
+Utils for basic calculations.
+"""
+
+
+def add(a: int, b: int) -> int:
+    """
+    This function adds two numbers.
+    """
     return a + b
 
-def subtract (a: int , b: int) -> int:
+
+def subtract(a: int, b: int) -> int:
+    """
+    This function subtract two numbers.
+    """
     return a - b
 
-def multiply (a: int , b: int) -> int:
+
+def multiply(a: int, b: int) -> int:
+    """
+    This function multiply two numbers
+    """
     return a * b
 
-def divide (a: int , b: int) -> float :
+
+def divide(a: int, b: int) -> float:
+    """
+    This function divide two numbers
+    """
     return a / b
